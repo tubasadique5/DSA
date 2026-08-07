1f8102842ef0557bba72f8999286fbc22c73649c
Tuba Sadique <tubasadique5@gmail.com>
2026-08-07 12:56:57 +0530

    Time: 8 ms (28.60%) | Memory: 12.8 MB (45.36%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        i=0
+        j=len(numbers)-1
+
+        while i<j:
+            s=numbers[i]+numbers[j]
+
+            if s==target:
+                return[i+1,j+1]
+            elif s<target:
+                i+=1
+            else:
+                j-=1    
+
+
+        
