05625430c0e88eff1ac7d06466f141e3295f06dd
Tuba Sadique <tubasadique5@gmail.com>
2026-08-31 20:03:58 +0530

    Time: 10 ms (51.84%) | Memory: 14.9 MB (38.46%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>f;
+        int i;
+        int result;
+        for(i=0;i<s.size();i++)
+        {
+         f[s[i]]++;
+        }
+        for(i=0;i<s.size();i++)
+        {
+            if(f[s[i]]==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
